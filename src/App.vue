5ba3bb0f9b10ddb88b2e13edb2d35067e1cea786
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-10-31 13:25:43 +0200

    le style des section manga est fini

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 //import HelloWorld from './components/HelloWorld.vue'
+import sectionManga from './components/sectionManga.vue';
 import barreTache from './components/barreTache.vue';
 const connected = false;
 </script>
@@ -7,8 +8,18 @@ const connected = false;
 <template>
   <barreTache :is-connected="connected" />
 
-  <div>
-
+  <div class="container">
+    <sectionManga />
   </div>
 </template>
 
+<style scoped>
+   .container{
+    display: flex;
+    /* flex-direction: row; */
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    /* margin-top: 100px; */
+  }
+</style>
