463e1c95a1e9b2cb327e2e30550faec65a077949
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-12-01 19:13:22 +0200

    nike

--- src/contact.ts
+++ src/contact.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import '../style.css'
-import contact from '../contact.vue'
+import './style.css'
+import contact from './contact.vue'
 
 createApp(contact).mount('#app')
--- src/signIn.ts
+++ src/signIn.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import '../style.css'
-import Signup from '../SignIn.vue'
+import './style.css'
+import Signup from './SignIn.vue'
 // import App from './App.vue'
 
 createApp(Signup).mount('#app')
