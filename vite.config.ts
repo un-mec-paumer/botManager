cf3b0fad4f9b5568c717e25fdbf5de311f7c14ce
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-12-03 23:03:46 +0200

    heroku raciste 8 (fin ?)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port:3000 || parseInt(process.env.PORT),
+  },
 })
