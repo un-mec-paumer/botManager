9ae3fb3b240f52cf42a26c62ba778803cfec08d9
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-12-03 23:11:39 +0200

    heroku raciste 10 aller merde

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,11 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     port:3000 || parseInt(PORT as string),
+    open: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': '*',
+    },
   },
 })
