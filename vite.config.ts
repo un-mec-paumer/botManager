7f033b33a6238c0462d71a1c4d7caba5954554bd
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-12-03 23:41:24 +0200

    heroku raciste 14 j'ycrois plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import * as dotenv from 'dotenv'
 
 dotenv.config()
-const PORT = parseInt(process.env.PORT as string)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: PORT || 3000,
+    port: Number(process.env.PORT) || 3000,
     open: false,
     headers: {
       'Access-Control-Allow-Origin': '*',
