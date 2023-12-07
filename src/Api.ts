143e0e3f60197f90c60938aed8de62c723f87d9b
CIRIC Antoine <antoine.ciric7@etu.univ-lorraine.fr>
2023-12-07 22:18:46 +0200

    ajout du fonctionement de addmanga

--- src/Api.ts
+++ src/Api.ts
@@ -1,2 +1,2 @@
-// export const api = "https://sleepy-atoll-80429-04066cba83f3.herokuapp.com"
-export const api = "http://localhost:3000"
+export const api = "https://sleepy-atoll-80429-04066cba83f3.herokuapp.com"
+// export const api = "http://localhost:3000"
